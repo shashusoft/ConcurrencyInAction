73bcc2524b4abeaf17408875a733ace419dcf3da
shashusoft <shashusoft@gmail.com>
2020-08-04 12:56:04 +0200

    functor issue resolved with uniform initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <thread>
 
+class ParallelFunctor
+{
+public:
+	void operator()()
+	{
+		doSomething();
+		doSomething_1();
+	}
+
+	void doSomething()
+	{
+		std::cout << "do Something " << std::endl;
+	}
+
+	void doSomething_1()
+	{
+		std::cout << "do Something 1 " << std::endl;
+	}
+};
+
 void parallelWorld()
 {
 	std::cout << "Parallel thread " << std::endl;
@@ -8,8 +28,10 @@ void parallelWorld()
 
 int main(int argc, char* argv[])
 {
-	std::thread t(parallelWorld);
+	std::thread t1(parallelWorld);
+	std::thread t2{ParallelFunctor()};
 	std::cout << "Main thread " << std::endl;
-	t.join();
+	t1.join();
+	t2.join();
 }
 
